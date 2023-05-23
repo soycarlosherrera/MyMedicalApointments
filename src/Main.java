34eefbad45becc418556fba0b114a4227a37eea1
Carlos Herrera <carlos4505@gmail.com>
2023-05-23 00:28:43 -0500

    Getters y Setters

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,11 @@ public class Main {
         System.out.println(myDoctor.speciality);
 
         Patient patient = new Patient("Alejandra","alejandra@gmail.com");
+        patient.setWeight(54.6);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("12345678");
+        System.out.println(patient.getPhoneNumber());
 
     }
 }

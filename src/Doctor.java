412edf7dc804b1a2d5614ceab788b7784b0ffbaa
Carlos Herrera <carlos4505@gmail.com>
2023-05-22 21:05:32 -0500

    Agrego clases y metodos static

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,10 +1,11 @@
 public class Doctor {
-    int id;
+    static int id=0;//Autoincrement
     String name;
     String speciality;
 
     Doctor(){
         System.out.println("Construyendo el Objeto Doctor");
+        id++;
     }
 
     Doctor(String name){
@@ -14,7 +15,12 @@ public class Doctor {
     //Comportamientos
 
     public void showName(){
+
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: "+id);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
-
+import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor("Carlos Herrera");
+        Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Rodriguez";
         myDoctor.showName();
+        myDoctor.showId();
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctorAnn.showId();
+
+        showMenu();
 
     }
 }

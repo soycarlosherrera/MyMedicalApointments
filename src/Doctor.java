3449a7ee7d91dd6ca9bd047a61faae7c39727072
Carlos Herrera <carlos4505@gmail.com>
2023-05-22 19:41:50 -0500

    Modificando constructor

--- src/Doctor.java
+++ src/Doctor.java
@@ -3,6 +3,14 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor(){
+        System.out.println("Construyendo el Objeto Doctor");
+    }
+
+    Doctor(String name){
+        System.out.println("El nombre del Doctor asignado es: "+name);
+    }
+
     //Comportamientos
 
     public void showName(){
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Carlos Herrera");
         myDoctor.name = "Alejandro Rodriguez";
         myDoctor.showName();
 

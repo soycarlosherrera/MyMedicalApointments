34d771165e01f1c0fc14bf0544a3d69fd2d25df8
Carlos Herrera <carlos4505@gmail.com>
2023-05-22 23:47:49 -0500

    Clase paciente

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,15 +1,17 @@
 public class Doctor {
     static int id=0;//Autoincrement
     String name;
+    String email;
     String speciality;
 
     Doctor(){
         System.out.println("Construyendo el Objeto Doctor");
-        id++;
     }
-
-    Doctor(String name){
+    Doctor(String name, String speciality ) {
         System.out.println("El nombre del Doctor asignado es: "+name);
+        id++;
+        this.name=name;
+        this.speciality=speciality;
     }
 
     //Comportamientos
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        showMenu();
+        //showMenu();
+        Doctor myDoctor = new Doctor("Carlos Herrera","Pediatria");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
+
+        Patient patient = new Patient("Alejandra","alejandra@gmail.com");
 
     }
 }

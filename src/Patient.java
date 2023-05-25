5e7ac24d40c9b156155e300b9c4708cecd2a71e0
Carlos Herrera <carlos4505@gmail.com>
2023-05-24 22:50:02 -0500

    Herencia Doctores y Pacientes

--- src/Patient.java
+++ src/Patient.java
@@ -1,61 +1,11 @@
-public class Patient {
-    int id;
-    private String name;
-    private String email;
-    private String address;
-    private String phoneNumber;
+public class Patient extends User{
     private String birthday;
     private Double weight;
     private Double height;
     private String blood;
 
     Patient(String name, String email){
-        this.name = name;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        if (phoneNumber.length()>8){
-            System.out.println("El numero telefonico debe ser de 8 digitos maximo");
-        } else if (phoneNumber.length()==8) {
-            this.phoneNumber = phoneNumber;
-        }
+        super(name, email);
     }
 
     public String getBirthday() {
